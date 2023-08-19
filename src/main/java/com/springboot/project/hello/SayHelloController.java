d64c554830f160d8d4389b401658fcfc7c52512d
Amelia <xinyi.wu@startupbootcamp.com.au>
2023-08-19 11:54:11 +1000

    added first controller

---
+++ src/main/java/com/springboot/project/hello/SayHelloController.java
@@ -0,0 +1,17 @@
+package com.springboot.project.hello;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    //http://localhost:8080/say-hello
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+}
